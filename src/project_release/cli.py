69aea3ce48a88b5200225d4efd5b8dd7de94577f
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-12-02 17:51:32 +0100

    WIP

--- src/project_release/cli.py
+++ src/project_release/cli.py
@@ -5,6 +5,7 @@ from typing import List
 from typing import Optional
 
 from . import __version__
+from .config import Config
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +31,13 @@ def project_release_cli(argv: Optional[List[str]] = None) -> int:
         "--version", action="version", version=f"%(prog)s {__version__}"
     )
     parser.add_argument("-v", "--verbose", action="store_true", help="show debug logs")
+    parser.add_argument(
+        "-c",
+        "--config",
+        default=".project-release-config.yaml",
+        type=str,
+        help="specify an alternate config file",
+    )
 
     args = parser.parse_args(args=argv)
 
@@ -37,5 +45,9 @@ def project_release_cli(argv: Optional[List[str]] = None) -> int:
         format="%(levelname)s: %(message)s",
         level=logging.DEBUG if args.verbose else logging.INFO,
     )
+
+    config = Config(args.config)
+
+    config.parse()
 
     return 0
