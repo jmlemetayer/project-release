c06ab29542a37b39ea179f643ce02856a333c1f1
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2023-01-29 00:02:06 +0100

    feat(cli): add initial checks

--- src/project_release/cli.py
+++ src/project_release/cli.py
@@ -1,12 +1,18 @@
 """The command line module."""
 import argparse
 import logging
+import pathlib
+import sys
 from typing import List
 from typing import Optional
+
+import git
 
 from . import __version__
 
 logger = logging.getLogger(__name__)
+
+CONFIG_FILE = ".project-release-config.yaml"
 
 
 def project_release_cli(argv: Optional[List[str]] = None) -> int:
@@ -31,11 +37,36 @@ def project_release_cli(argv: Optional[List[str]] = None) -> int:
     )
     parser.add_argument("-v", "--verbose", action="store_true", help="show debug logs")
 
+    parser.add_argument(
+        "-c",
+        "--config",
+        default=CONFIG_FILE,
+        help="specify an alternate configuration file",
+    )
+
     args = parser.parse_args(args=argv)
 
     logging.basicConfig(
         format="%(levelname)s: %(message)s",
         level=logging.DEBUG if args.verbose else logging.INFO,
     )
+
+    try:
+        git_repo = git.Repo(search_parent_directories=True)
+    except git.InvalidGitRepositoryError:
+        sys.exit("Not in a git repository")
+
+    if git_repo.is_dirty():
+        sys.exit("The git repository is dirty")
+
+    git_dir = pathlib.Path(git_repo.git_dir)
+
+    config_file = git_dir.parent / args.config
+
+    if not config_file.exists():
+        sys.exit("Configuration file not found")
+
+    if not config_file.is_file():
+        sys.exit("The configuration file is not a regular file")
 
     return 0
