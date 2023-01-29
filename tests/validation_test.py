1ce607aba33680b7e8773aa8f92a73a72d8699f7
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2023-01-29 11:48:48 +0100

    test(convention): add accept all tests

--- tests/validation_test.py
+++ tests/validation_test.py
@@ -2,11 +2,21 @@
 import logging
 
 import pytest
+from project_release.validation import AcceptAllValidator
 from project_release.validation import InvalidVersionError
 from project_release.validation import Pep440Validator
 from project_release.validation import SemverValidator
 
 logger = logging.getLogger(__name__)
+
+
+class TestAcceptAllValidator:
+    """Test cases related to the accept all validator."""
+
+    def test_valid(self) -> None:
+        """Test that a valid version is valid."""
+        assert AcceptAllValidator().validate("1.2.3+devel")
+        assert AcceptAllValidator().validate("1.2.3.dev1")
 
 
 class TestSemverValidator:
