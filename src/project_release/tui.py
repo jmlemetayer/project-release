6c4ba4d21d07fb2a563a4f67f5434d89faa08e43
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2023-04-01 21:02:40 +0200

    feat(cli): select development and release branches

--- src/project_release/tui.py
+++ src/project_release/tui.py
@@ -1,6 +1,8 @@
 """Text-based user interface (TUI) related code."""
+import fnmatch
 import logging
 from typing import Callable
+from typing import List
 from typing import Optional
 from typing import Union
 
@@ -41,6 +43,58 @@ def select_remote(repo: git.Repo, user_remote: Optional[str]) -> Optional[str]:
         return available_remotes[0]
     return questionary.select(
         "Select the git remote to use", choices=[x.name for x in repo.remotes]
+    ).unsafe_ask()
+
+
+def select_branch(
+    branch_name: str,
+    config_branches: List[str],
+    user_branch: Optional[str],
+) -> str:
+    """Select the git branch to use.
+
+    Parameters
+    ----------
+    branch_name
+        The name of the branch.
+    config_branches
+        The git branches available in configuration.
+    user_branch
+        The user-specified git branch.
+
+    Returns
+    -------
+    str
+        The selected git branch.
+
+    Raises
+    ------
+    SystemExit
+        If the specified git branch is invalid.
+    """
+
+    def is_pattern_branch(branch: str) -> bool:
+        return any(c in branch for c in ["*", "?", "["])
+
+    plain_branches = [b for b in config_branches if not is_pattern_branch(b)]
+    pattern_branches = [b for b in config_branches if b not in plain_branches]
+
+    def validate_branch(branch: str) -> Union[bool, str]:
+        if branch in plain_branches:
+            return True
+        for pattern in pattern_branches:
+            if fnmatch.fnmatchcase(branch, pattern):
+                return True
+        return f"Invalid branch name: '{branch}'"
+
+    if user_branch is not None:
+        if validate_branch(user_branch) is not True:
+            raise SystemExit(validate_branch(user_branch))
+        return user_branch
+    if not pattern_branches and len(plain_branches) == 1:
+        return plain_branches[0]
+    return questionary.text(
+        f"Specify the desired {branch_name} branch", validate=validate_branch
     ).unsafe_ask()
 
 
