935de21b4c0e0bd8f21d07b4d1986dc7119c0e10
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2024-03-08 15:32:56 +0100

    feat(utils): add a relative_path helper function

--- src/project_release/utils.py
+++ src/project_release/utils.py
@@ -1,5 +1,7 @@
 """Simple utility functions."""
 import logging
+from pathlib import Path
+from typing import Union
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +19,13 @@ def log_exception(exc: BaseException, level: int = logging.ERROR) -> None:
     if exc.__cause__:
         log_exception(exc.__cause__, level)
     logger.log(level, str(exc))
+
+
+def relative_path(path: Union[Path, str]) -> Path:
+    """Get the relative path from the current directory if possible."""
+    if not isinstance(path, Path):
+        path = Path(path)
+    try:
+        return path.relative_to(Path.cwd())
+    except ValueError:
+        return path
